2c9de7b61202d8ae7b285352f970709e09207cf1
starford gwala <starfordgwala@gmail.com>
2018-09-25 22:56:40 +0300

    single file without goroutine

--- masanduku/README.md
+++ masanduku/README.md
@@ -5,3 +5,10 @@
 The Task
 
 > From a common excel sheet of 100,000 records (11 columns). Read the contents of the file then order the fathers_name column alphabetically --NOT THE RECORD, JUST THE COLUMN--- Insert into a MySQL Database then create a new Excel file with the new reordered data.
+
+
+### note
+currently reading the file takes over a minute
+but work in progress
+
+using the [excelize](github.com/360EntSecGroup-Skylar/excelize) to read and write the excel file
--- masanduku/main.go
+++ masanduku/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"sort"
+	"strconv"
 	"time"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
 /* the excel names and sheet name */
-const filename string = "samplefile.xlsx"
-const sheetname string = "sample"
+// const filename string = "samplefile.xlsx"
+// const sheetname string = "sample"
+
+const filename string = "100000RecordsFull.xlsx"
+const sheetname string = "100000 Records"
 
 func main() {
 	start := time.Now()
+
 	//read the excel file
 	xlsx, err := excelize.OpenFile(filename)
 	if err != nil {
@@ -22,8 +28,10 @@ func main() {
 	}
 	//get the rows
 	rows := xlsx.GetRows(sheetname)
+
+	//this is the header
 	//header := rows[0]
-	//fmt.Println(header)
+	//remove the headers
 	noheaderarray := append(rows[:0], rows[1:]...)
 
 	//sort the fathers name column values
@@ -34,23 +42,49 @@ func main() {
 		fathersnamearray = append(fathersnamearray, row[7])
 	}
 	sort.Strings(fathersnamearray)
-	//fmt.Println(fathersnamearray)
 
-	//create new rows of data
-
-	//fmt.Println(fathersnamearray)
-	fmt.Printf("Lenghth of data in fathersnames array: %d, Cap: %d \n", len(fathersnamearray), cap(fathersnamearray))
+	//fmt.Printf("Lenghth of data in fathersnames array: %d, Cap: %d \n", len(fathersnamearray), cap(fathersnamearray))
 
 	//fmt.Println(noheaderarray)
-	fmt.Printf("Lenghth of data in all the data array: %d, Cap: %d \n", len(noheaderarray), cap(noheaderarray))
+	//fmt.Printf("Lenghth of data in all the data array: %d, Cap: %d \n", len(noheaderarray), cap(noheaderarray))
 
-	for index := range noheaderarray {
-		fmt.Println(fathersnamearray[index], noheaderarray[index])
-	}
+	//loop and create the new csv
+	//fmt.Println(header)
+	// for index := range noheaderarray {
+	// 	fmt.Println(fathersnamearray[index])
+	// 	fmt.Println(noheaderarray[index])
+	// }
 
-	//rows = rows[len(rows)-1]
+	//fmt.Printf("Lenghth of data in all the data array: %d, Cap: %d \n", len(rows), cap(rows))
 
-	//fmt.Println(rows[0][7])
+	donerunning := make(chan bool)
+	go func() {
+
+		ignoreheadercounter := 1
+		for i := 0; i < len(rows)-1; i++ {
+			// fmt.Println(fathersnamearray[i])
+			// fmt.Println(noheaderarray[i])
+
+			indexstring := strconv.Itoa(ignoreheadercounter + 1)
+			//stringbuffer := bytes.Buffer
+			var stringbuffer bytes.Buffer
+			stringbuffer.WriteString("L")
+			stringbuffer.WriteString(indexstring)
+
+			//fmt.Println(stringbuffer.String())
+
+			xlsx.SetCellValue(sheetname, stringbuffer.String(), fathersnamearray[i])
+
+			ignoreheadercounter++
+		}
+		xlsx.Save()
+		donerunning <- true
+
+	}()
+	finito := <-donerunning
+	// fmt.Println("imefika")
+	fmt.Println("finito", finito)
 
 	fmt.Printf("took %v\n", time.Since(start))
+
 }
