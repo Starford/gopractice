784b7ecd29a14209c3ab7745867209960a42c02d
starford gwala <starfordgwala@gmail.com>
2018-10-25 21:34:27 +0300

    using goroutines to sort fathersnames from excel

--- masanduku/main.go
+++ masanduku/main.go
@@ -1,22 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"sort"
-	"strconv"
 	"time"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
 /* the excel names and sheet name */
-// const filename string = "samplefile.xlsx"
-// const sheetname string = "sample"
+const filename string = "samplefile.xlsx"
+const sheetname string = "sample"
 const newfilename string = "output.xlsx"
 
-const filename string = "100000RecordsFull.xlsx"
-const sheetname string = "100000 Records"
+// const filename string = "100000RecordsFull.xlsx"
+// const sheetname string = "100000 Records"
 
 func main() {
 	start := time.Now()
@@ -30,8 +27,6 @@ func main() {
 	//get the rows
 	rows := xlsx.GetRows(sheetname)
 
-	//this is the header
-	//header := rows[0]
 	//remove the headers
 	noheaderarray := append(rows[:0], rows[1:]...)
 
@@ -42,52 +37,72 @@ func main() {
 		//get the fathers name column
 		fathersnamearray = append(fathersnamearray, row[7])
 	}
-	sort.Strings(fathersnamearray)
+	fmt.Println("fathers names BEFORE sort", fathersnamearray)
+	//sort.Strings(fathersnamearray)
+	fathersnamechan := make(chan []string)
+	//fathersnamechan <- fathersnamearray
+	mergerSortAsync(fathersnamearray, fathersnamechan)
 
-	//fmt.Printf("Lenghth of data in fathersnames array: %d, Cap: %d \n", len(fathersnamearray), cap(fathersnamearray))
-
-	//fmt.Println(noheaderarray)
-	//fmt.Printf("Lenghth of data in all the data array: %d, Cap: %d \n", len(noheaderarray), cap(noheaderarray))
-
-	//loop and create the new csv
-	//fmt.Println(header)
-	// for index := range noheaderarray {
-	// 	fmt.Println(fathersnamearray[index])
-	// 	fmt.Println(noheaderarray[index])
-	// }
-
-	//fmt.Printf("Lenghth of data in all the data array: %d, Cap: %d \n", len(rows), cap(rows))
-
-	// donerunning := make(chan bool)
-	// go func() {
-	//index := xlsx.NewSheet(sheetname)
-	//xlsx.SetCellValue(sheetname, "A1", "Fathers name")
-	//xlsx.SetCellValue("Sheet1", "B2", 100)
-	/* excel title */
-
-	ignoreheadercounter := 1
-	for i := 0; i < len(rows)-1; i++ {
-		// fmt.Println(fathersnamearray[i])
-		// fmt.Println(noheaderarray[i])
-
-		indexstring := strconv.Itoa(ignoreheadercounter + 1)
-		//to concat the letter
-		var stringbuffer bytes.Buffer
-		stringbuffer.WriteString("H")
-		stringbuffer.WriteString(indexstring)
-
-		xlsx.SetCellValue(sheetname, stringbuffer.String(), fathersnamearray[i])
-
-		ignoreheadercounter++
-	}
-	xlsx.SaveAs(newfilename)
-	//donerunning <- true
-
-	//}()
-	//finito := <-donerunning
-	// fmt.Println("imefika")
-	//fmt.Println("finito", finito)
+	//sortedStuff := <-fathersnamechan
+	fmt.Println("fathers names after sort")
 
 	fmt.Printf("took %v\n", time.Since(start))
 
+}
+
+func mergerSortAsync(l []string, c chan []string) {
+	//commented below because am not sure what it is doing exactly
+	// if len(l) < 2 {
+	// 	c <- l
+	// 	return
+	// }
+	// if len(l) < 500 { //TUNE THIS NUMER AND DONT CREATE EXTRA WORK UNLESS IT'S BIGGER
+	// 	c <- mergeSort(l)
+	// 	return
+	// }
+
+	mid := len(l) / 2
+	c1 := make(chan []string, 1)
+	c2 := make(chan []string, 1)
+
+	go mergerSortAsync(l[:mid], c1)
+	go mergerSortAsync(l[mid:], c2)
+
+	go func() { c <- merge(<-c1, <-c2) }()
+
+}
+
+func mergeSort(l []string) []string {
+	if len(l) < 2 {
+		return l
+	}
+	mid := len(l) / 2
+	a := mergeSort(l[:mid])
+	b := mergeSort(l[mid:])
+	return merge(a, b)
+}
+
+func merge(left, right []string) []string {
+	var i, j int
+	result := make([]string, len(left)+len(right))
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result[i+j] = left[i]
+			i++
+		} else {
+			result[i+j] = right[j]
+			j++
+		}
+	}
+
+	for i < len(left) {
+		result[i+j] = left[i]
+		i++
+	}
+	for j < len(right) {
+		result[i+j] = right[j]
+		j++
+	}
+	return result
 }
